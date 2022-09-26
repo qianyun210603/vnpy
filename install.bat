79eef65378d19af0941b27a2d493426ac5cc2c4c
BookSword <qianyun210603@hotmail.com>
2022-09-26 19:22:31 +0800

    remove version restriction

--- install.bat
+++ install.bat
@@ -5,7 +5,7 @@ python -m pip install --upgrade pip wheel
 python -m pip install --extra-index-url http://139.196.190.180 --trusted-host 139.196.190.180 TA_Lib
 
 ::Degrade setuptools to install deap 1.3.1 for python3.10
-python -m pip install setuptools==57.0.0
+python -m pip install setuptools
 
 ::Install Python Modules
 python -m pip install -r requirements.txt
