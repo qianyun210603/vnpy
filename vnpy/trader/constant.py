379221ffcbac72a75aaf8711cd15d94a3a0570e4
qianyun6 <qianyun210603@hotmail.com>
2024-03-05 19:57:36 +0800

    add method in direction to calculate opposite

--- vnpy/trader/constant.py
+++ vnpy/trader/constant.py
@@ -9,15 +9,24 @@ class Direction(Enum):
     """
     Direction of order/trade/position.
     """
+
     LONG = "多"
     SHORT = "空"
     NET = "净"
+
+    def opposite(self):
+        if self == Direction.LONG:
+            return Direction.SHORT
+        elif self == Direction.SHORT:
+            return Direction.LONG
+        return Direction.NET
 
 
 class Offset(Enum):
     """
     Offset of order/trade.
     """
+
     NONE = ""
     OPEN = "开"
     CLOSE = "平"
@@ -29,6 +38,7 @@ class Status(Enum):
     """
     Order status.
     """
+
     SUBMITTING = "提交中"
     NOTTRADED = "未成交"
     PARTTRADED = "部分成交"
@@ -41,6 +51,7 @@ class Product(Enum):
     """
     Product class.
     """
+
     EQUITY = "股票"
     FUTURES = "期货"
     OPTION = "期权"
@@ -58,6 +69,7 @@ class OrderType(Enum):
     """
     Order type.
     """
+
     LIMIT = "限价"
     MARKET = "市价"
     STOP = "STOP"
@@ -70,6 +82,7 @@ class OptionType(Enum):
     """
     Option type.
     """
+
     CALL = "看涨期权"
     PUT = "看跌期权"
 
@@ -78,69 +91,71 @@ class Exchange(Enum):
     """
     Exchange.
     """
+
     # Chinese
-    CFFEX = "CFFEX"         # China Financial Futures Exchange
-    SHFE = "SHFE"           # Shanghai Futures Exchange
-    CZCE = "CZCE"           # Zhengzhou Commodity Exchange
-    DCE = "DCE"             # Dalian Commodity Exchange
-    INE = "INE"             # Shanghai International Energy Exchange
-    GFEX = "GFEX"           # Guangzhou Futures Exchange
-    SSE = "SSE"             # Shanghai Stock Exchange
-    SZSE = "SZSE"           # Shenzhen Stock Exchange
-    BSE = "BSE"             # Beijing Stock Exchange
-    SHHK = "SHHK"           # Shanghai-HK Stock Connect
-    SZHK = "SZHK"           # Shenzhen-HK Stock Connect
-    SGE = "SGE"             # Shanghai Gold Exchange
-    WXE = "WXE"             # Wuxi Steel Exchange
-    CFETS = "CFETS"         # CFETS Bond Market Maker Trading System
-    XBOND = "XBOND"         # CFETS X-Bond Anonymous Trading System
+    CFFEX = "CFFEX"  # China Financial Futures Exchange
+    SHFE = "SHFE"  # Shanghai Futures Exchange
+    CZCE = "CZCE"  # Zhengzhou Commodity Exchange
+    DCE = "DCE"  # Dalian Commodity Exchange
+    INE = "INE"  # Shanghai International Energy Exchange
+    GFEX = "GFEX"  # Guangzhou Futures Exchange
+    SSE = "SSE"  # Shanghai Stock Exchange
+    SZSE = "SZSE"  # Shenzhen Stock Exchange
+    BSE = "BSE"  # Beijing Stock Exchange
+    SHHK = "SHHK"  # Shanghai-HK Stock Connect
+    SZHK = "SZHK"  # Shenzhen-HK Stock Connect
+    SGE = "SGE"  # Shanghai Gold Exchange
+    WXE = "WXE"  # Wuxi Steel Exchange
+    CFETS = "CFETS"  # CFETS Bond Market Maker Trading System
+    XBOND = "XBOND"  # CFETS X-Bond Anonymous Trading System
 
     # Global
-    SMART = "SMART"         # Smart Router for US stocks
-    NYSE = "NYSE"           # New York Stock Exchnage
-    NASDAQ = "NASDAQ"       # Nasdaq Exchange
-    ARCA = "ARCA"           # ARCA Exchange
-    EDGEA = "EDGEA"         # Direct Edge Exchange
-    ISLAND = "ISLAND"       # Nasdaq Island ECN
-    BATS = "BATS"           # Bats Global Markets
-    IEX = "IEX"             # The Investors Exchange
-    AMEX = "AMEX"           # American Stock Exchange
-    TSE = "TSE"             # Toronto Stock Exchange
-    NYMEX = "NYMEX"         # New York Mercantile Exchange
-    COMEX = "COMEX"         # COMEX of CME
-    GLOBEX = "GLOBEX"       # Globex of CME
-    IDEALPRO = "IDEALPRO"   # Forex ECN of Interactive Brokers
-    CME = "CME"             # Chicago Mercantile Exchange
-    ICE = "ICE"             # Intercontinental Exchange
-    SEHK = "SEHK"           # Stock Exchange of Hong Kong
-    HKFE = "HKFE"           # Hong Kong Futures Exchange
-    SGX = "SGX"             # Singapore Global Exchange
-    CBOT = "CBT"            # Chicago Board of Trade
-    CBOE = "CBOE"           # Chicago Board Options Exchange
-    CFE = "CFE"             # CBOE Futures Exchange
-    DME = "DME"             # Dubai Mercantile Exchange
-    EUREX = "EUX"           # Eurex Exchange
-    APEX = "APEX"           # Asia Pacific Exchange
-    LME = "LME"             # London Metal Exchange
-    BMD = "BMD"             # Bursa Malaysia Derivatives
-    TOCOM = "TOCOM"         # Tokyo Commodity Exchange
-    EUNX = "EUNX"           # Euronext Exchange
-    KRX = "KRX"             # Korean Exchange
-    OTC = "OTC"             # OTC Product (Forex/CFD/Pink Sheet Equity)
-    IBKRATS = "IBKRATS"     # Paper Trading Exchange of IB
-    AEB = "AEB"             # Amsterdam Exchange
+    SMART = "SMART"  # Smart Router for US stocks
+    NYSE = "NYSE"  # New York Stock Exchnage
+    NASDAQ = "NASDAQ"  # Nasdaq Exchange
+    ARCA = "ARCA"  # ARCA Exchange
+    EDGEA = "EDGEA"  # Direct Edge Exchange
+    ISLAND = "ISLAND"  # Nasdaq Island ECN
+    BATS = "BATS"  # Bats Global Markets
+    IEX = "IEX"  # The Investors Exchange
+    AMEX = "AMEX"  # American Stock Exchange
+    TSE = "TSE"  # Toronto Stock Exchange
+    NYMEX = "NYMEX"  # New York Mercantile Exchange
+    COMEX = "COMEX"  # COMEX of CME
+    GLOBEX = "GLOBEX"  # Globex of CME
+    IDEALPRO = "IDEALPRO"  # Forex ECN of Interactive Brokers
+    CME = "CME"  # Chicago Mercantile Exchange
+    ICE = "ICE"  # Intercontinental Exchange
+    SEHK = "SEHK"  # Stock Exchange of Hong Kong
+    HKFE = "HKFE"  # Hong Kong Futures Exchange
+    SGX = "SGX"  # Singapore Global Exchange
+    CBOT = "CBT"  # Chicago Board of Trade
+    CBOE = "CBOE"  # Chicago Board Options Exchange
+    CFE = "CFE"  # CBOE Futures Exchange
+    DME = "DME"  # Dubai Mercantile Exchange
+    EUREX = "EUX"  # Eurex Exchange
+    APEX = "APEX"  # Asia Pacific Exchange
+    LME = "LME"  # London Metal Exchange
+    BMD = "BMD"  # Bursa Malaysia Derivatives
+    TOCOM = "TOCOM"  # Tokyo Commodity Exchange
+    EUNX = "EUNX"  # Euronext Exchange
+    KRX = "KRX"  # Korean Exchange
+    OTC = "OTC"  # OTC Product (Forex/CFD/Pink Sheet Equity)
+    IBKRATS = "IBKRATS"  # Paper Trading Exchange of IB
+    AEB = "AEB"  # Amsterdam Exchange
 
-    OKEXD = "OKEXD"         # okex derivative
-    OKEXS = "OKEXS"         # okex spot
+    OKEXD = "OKEXD"  # okex derivative
+    OKEXS = "OKEXS"  # okex spot
 
     # Special Function
-    LOCAL = "LOCAL"         # For local generated data
+    LOCAL = "LOCAL"  # For local generated data
 
 
 class Currency(Enum):
     """
     Currency.
     """
+
     USD = "USD"
     HKD = "HKD"
     CNY = "CNY"
@@ -152,6 +167,7 @@ class Interval(Enum):
     """
     Interval of bar data.
     """
+
     MINUTE = "1m"
     HOUR = "1h"
     DAILY = "d"
